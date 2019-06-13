cd7aef34551733cd36c948e79a2201fa8bd3fbb3
trusohamn <marta.trusohamn@gmail.com>
2019-06-13 22:08:08 +0200

    build

---
+++ build/precache-manifest.a8a6a3167188ada8234e5a60483c9388.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6654b081f8c77a6c0ae53c85cd1c6461",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af665e8d53f8395f4702",
+    "url": "/static/css/main.9d5f2d92.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "af665e8d53f8395f4702",
+    "url": "/static/js/main.447bc851.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
